4571d25dc85dc455296de881aa36a09a08923a30
Théo m <>
2023-11-20 14:38:54 +0100

    fix : requête en trop supprimé

--- src/App.js
+++ src/App.js
@@ -1,33 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
-import axios from "axios";
 import Data from "./composants/Data";
 
-const setting = axios.get('http://192.168.1.100/settings')
-    .then(function (response) {
-    })
-    .catch(function (error) {
-      console.log(error);
-    })
+// const setting = axios.get('http://192.168.1.100/settings')
+//  let setting = null;
+//  axios.get('https://shelly-77-eu.shelly.cloud/device/status?id=4022d88e30e8&auth_key=MWNiMjY5dWlk404459961993DCA83AE44BC6E3A6F58906952E7BECA0A5B69DC375C964915ACBC0EA536A0639CB73')
+//     .then(function (response) {
+//         setting = response;
+//       console.log("status", response);
+//     })
+//     .catch(function (error) {
+//       console.log(error);
+//     })
 
-const status = axios.get('http://192.168.1.100/status')
-    .then(function (response) {
-    })
-    .catch(function (error) {
-        console.log(error);
-    })
-
-const actions = axios.get('http://192.168.1.100/settings/actions')
-    .then(function (response) {
-    })
-    .catch(function (error) {
-        console.log(error);
-    })
+// const status = axios.get('http://192.168.1.100/status')
+//     .then(function (response) {
+//         // console.log("status", response);
+//     })
+//     .catch(function (error) {
+//         console.log(error);
+//     })
+//
+// const actions = axios.get('http://192.168.1.100/settings/actions')
+//     .then(function (response) {
+//         // console.log("actions", response);
+//     })
+//     .catch(function (error) {
+//         console.log(error);
+//     })
 
 
 function App() {
-
-
   return (
     <div className="App">
       <header className="App-header">
--- src/composants/Data.js
+++ src/composants/Data.js
@@ -3,32 +3,62 @@ import axios from "axios";
 
 
 function Data() {
-    const [ison, setIson] = useState("off")
+    const [apiData, setApiData] = useState({});
     const [etat, setEtat] = useState(false);
-    async function toggle(ison) {
-        try {
-            const response = await axios.post(`http://192.168.1.100/relay/0?turn=${etat ? 'on' : 'off'}`);
-        } catch (error) {
-            console.error(error);
-        }
-    }
-    const toggleEtat = () => {
+    const id = "4022d88e30e8";
+    const auth_key = "MWNiMjY5dWlk404459961993DCA83AE44BC6E3A6F58906952E7BECA0A5B69DC375C964915ACBC0EA536A0639CB73";
+
+    useEffect( () => {
+        axios.get("https://shelly-77-eu.shelly.cloud/device/status?id="+id+"&auth_key="+auth_key).then(async (response) => {
+            await setApiData(response.data.data.device_status);
+            setEtat(response.data.data.device_status.relays[0].ison);
+        });
+    }, [])
+
+    const switchEtat = () => {
+        const FormData = require('form-data');
+        let data = new FormData();
+        data.append('id', id);
+        data.append('turn', etat ? 'off' : 'on');
+        data.append('auth_key', auth_key);
+        data.append('channel', '0');
+
+        const headers = data.getHeaders ? data.getHeaders() : { 'Content-Type': 'multipart/form-data' };
+        let config = {
+            method: 'post',
+            maxBodyLength: Infinity,
+            url: 'https://shelly-77-eu.shelly.cloud/device/relay/control',
+            headers: {
+                ...headers
+            },
+            data : data
+        };
+        axios.request(config)
+            .then((response) => {
+                console.log(JSON.stringify(response.data));
+            })
+            .catch((error) => {
+                console.log(error);
+            });
         setEtat(!etat);
-        toggle(ison)
-    }
+    };
 
     return (
         <div>
             <div>
-            {etat ? (
+            {etat === true ? (
                 <img src="images/sun.svg" alt="sun" />
             ) : (
                 <img src="images/moon.svg" alt="moon" />
             )}
             </div>
-            <button onClick={toggleEtat}>
+            <button onClick={switchEtat}>
                SWITCH
             </button>
+            <div>
+                <p><u>Overtemperature</u> : {apiData.overtemperature ? 'Oui' : 'Non'}</p>
+                <p><u>Temperature</u> : {apiData.temperature}</p>
+            </div>
         </div>
     );
 }
